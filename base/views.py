80803f698749e6e230c52637235eb2a56205909b
neerajmanivarnan <neerajmanivarnan666@gmail.com>
2024-02-09 22:57:12 +0530

    urls edited

--- base/views.py
+++ base/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return render(request,'home.html')
+rooms = [
+    {'id' : '1','name':'Learn Python'},
+    {'id' : '2','name':'Spring Boot'},
+    {'id' : '3','name':'Learn React'}
+]
 
-def room(request):
-    return render(request,'room.html')  
+def home(request):
+    context = {'room':rooms}
+    return render(request,'base/home.html',context)
+
+def room(request, pk):
+    rom = None
+    for i in rooms:
+        if i['id'] == pk:
+            rom = i
+
+    context = {'room':rom}        
+    return render(request,'base/room.html',context)  
 
 
 # Create your views here.
