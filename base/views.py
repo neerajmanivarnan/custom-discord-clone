abeac91d39871fefc79ea1cbc871fd84fcfa7230
neerajmanivarnan <neerajmanivarnan666@gmail.com>
2024-02-11 10:43:16 +0530

    new commits

--- base/views.py
+++ base/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-rooms = [
-    {'id' : '1','name':'Learn Python'},
-    {'id' : '2','name':'Spring Boot'},
-    {'id' : '3','name':'Learn React'}
-]
+# rooms = [
+#     {'id' : '1','name':'Learn Python'},
+#     {'id' : '2','name':'Spring Boot'},
+#     {'id' : '3','name':'Learn React'}
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'room':rooms}
     return render(request,'base/home.html',context)
 
 def room(request, pk):
-    rom = None
-    for i in rooms:
-        if i['id'] == pk:
-            rom = i
+    rooms = Room.objects.get(id=pk)
+    # rom = None
+    # for i in rooms:
+    #     if i['id'] == pk:
+    #         rom = i
 
-    context = {'room':rom}        
+    context = {'room':rooms}        
     return render(request,'base/room.html',context)  
 
 
