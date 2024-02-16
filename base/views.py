ca9c05fe9e4c89f2763d8d205371919e6593f9d0
neerajmanivarnan <neerajmanivarnan666@gmail.com>
2024-02-16 17:46:18 +0530

    folder changed

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 # rooms = [
 #     {'id' : '1','name':'Learn Python'},
@@ -23,5 +24,26 @@ def room(request, pk):
     context = {'room':rooms}        
     return render(request,'base/room.html',context)  
 
+def createRoom(request):
+    form = RoomForm()
 
-# Create your views here.
+    if request.method == 'POST':
+        form=RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request,'base/room_form.html',context)
+
+
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request,'base/room_form.html',context)
